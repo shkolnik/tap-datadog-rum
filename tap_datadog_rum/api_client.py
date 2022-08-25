40bdd0581da87f3797fa8ebd3c52bed875fbb6e6
James Shkolnik <james.shkolnik@gusto.com>
2022-08-25 16:44:43 -0500

    Wait and retry when DD rate limit is reached.

--- tap_datadog_rum/api_client.py
+++ tap_datadog_rum/api_client.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timezone
+from time import sleep
+import sys
 
 from datadog_api_client import ApiClient, Configuration
-from datadog_api_client.exceptions import ApiAttributeError
+from datadog_api_client.exceptions import ApiAttributeError, ApiException
 from datadog_api_client.v2.api.rum_api import RUMApi
 
 PAGE_SIZE = 100
+MAX_RETRIES = 5
 DEFAULT_ATTRIBUTE_MAPPINGS = {
     'app_env': 'attributes.tags.env',
     'app_version': 'attributes.tags.version',
@@ -91,6 +94,27 @@ def format_event(event, config_attribute_mappings):
 def format_events(events, config_attribute_mappings):
     return list(map(lambda evt: format_event(evt, config_attribute_mappings), events))
 
+def list_events_with_retries(client, params):
+    api_instance = RUMApi(client)
+    for _prev_tries in range(0, MAX_RETRIES):
+        try:
+            return api_instance.list_rum_events(**params)
+        except ApiException as err:
+            my_err = err
+            if err.status != 429: # Datadog returns 429 for rate limit
+                raise err # Re-raise anything other than a rate limit
+
+            try:
+                rate_limit_reset_sec = int(err.headers['x-ratelimit-reset'])
+            except (KeyError, ValueError):
+                rate_limit_reset_sec = 30
+
+            print(f'Datadog rate limit reached, sleeping for {rate_limit_reset_sec}s', file=sys.stderr)
+            sleep(rate_limit_reset_sec)
+
+    raise 'Still hitting Datadog rate limit after all retries exhausted'
+
+
 
 class RUMApiClient:
     def __init__(self, api_key, app_key, start_date):
@@ -101,7 +125,6 @@ class RUMApiClient:
 
     def fetch_events(self, query, config_attribute_mappings, cursor = None, newest_first = False):
         with ApiClient(self.configuration) as api_client:
-            api_instance = RUMApi(api_client)
             params = {
                 'filter_query': query,
                 'filter_from': self.start_date,
@@ -111,7 +134,7 @@ class RUMApiClient:
             if cursor:
                 params['page_cursor'] = cursor
 
-            response = api_instance.list_rum_events(**params)
+            response = list_events_with_retries(api_client, params)
             formatted = format_events(response.data, config_attribute_mappings)
 
             try:
